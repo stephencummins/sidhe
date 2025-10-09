40daf4b3033304d69744d50ecab67c1385cc2d28
stephencummins <stephencummins@gmail.com>
2025-10-09 12:48:11 +0100

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'sidhe-navy': '#1a2f4a',
+        'sidhe-deep-blue': '#0d1b2a',
+        'sidhe-gold': '#d4af37',
+        'sidhe-bright-gold': '#f4c542',
+        'sidhe-orange': '#d35f3a',
+        'sidhe-coral': '#e07856',
+        'sidhe-teal': '#4a7c7e',
+        'sidhe-sage': '#6b8e8f',
+        'sidhe-cream': '#f5f1e8',
+        'sidhe-moon': '#e8e4d9',
+      },
+    },
   },
   plugins: [],
 };
